705d922da6c0753807ad752376bef12cdf8b5ecb
sleepielle <pggisselle@gmail.com>
2025-10-01 16:52:06 -0600

    feat: add allowedorigin for cors

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 8800;
 connectDB();
 
 // cors config
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://idea-drop-pink.vercel.app/",
+];
 
 //starting middleware
 app.use(cors({ origin: allowedOrigins, credentials: true }));
